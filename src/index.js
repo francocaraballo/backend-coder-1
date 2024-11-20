5da646e6a2043ca3b5d3dce77dc133f13633c281
Franco Caraballo <franconcaraballo@gmail.com>
2024-11-19 23:42:53 -0300

    Products routes

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-
 const PORT = 8080;
 
-// enpoint de telemetria
-app.get('/ping', (req, res) => {
-    res.send('pong');
-})
-
 // ROUTER
-
 app.use("/api/products", productsRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en el puerto ${PORT}`);
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -5,10 +5,76 @@ const router = Router();
 
 const productManager = new ProductManager();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     const limit = parseInt(req.query.limit);
-    const products =  await productManager.getAllProducts(limit);
-    res.send(products);
+    return res.send(productManager.products);
 })
 
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const productById = productManager.products.find(product => product.id === id);
+
+    if (!productById) return res.status(404).send('The product does not exist');
+    return res.send(productById);
+});
+
+router.post('/', (req, res) => {
+    const {
+        title,
+        description, 
+        code, 
+        price, 
+        status, 
+        stock, 
+        category, 
+        thumbnails 
+    } = req.body;
+
+    // VALIDAR QUE TODAS LAS PROPS SON ENVIADAS
+
+
+    const newProduct = {
+        title,
+        description, 
+        code, 
+        price, 
+        status, 
+        stock, 
+        category, 
+        thumbnails 
+    };
+
+    productManager.addProduct(newProduct);
+
+    return res.status(201).send(newProduct);
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.id);
+
+        const updatedProduct = await productManager.updateProduct(productId, req.body);
+        if (updatedProduct) {
+            res.json(updatedProduct);
+        } else {
+            res.status(404).json({ error: 'Product not found' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.id);
+        const deletedProduct = await productManager.deleteProduct(productId);
+        if (deletedProduct) {
+            res.json(deletedProduct);
+        } else {
+            res.status(404).json({ error: 'Product not found' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
 export default router;
